349e54115631f15b1927ba3157c840bd72c51a61
RaunaqSuri <raunaq.suri@hotmail.com>
2014-04-09 11:38:00 -0400

    Got xml reading working

--- workflowSpoofing/src/main/java/ca/on/oicr/pipedev/common/utilities/WorkflowSpoofing.java
+++ workflowSpoofing/src/main/java/ca/on/oicr/pipedev/common/utilities/WorkflowSpoofing.java
@@ -1,13 +1,42 @@
 package ca.on.oicr.pipedev.common.utilities;
 
 /**
- * Hello world!
+ * @author Raunaq Suri
  *
  */
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+
 public class WorkflowSpoofing 
 {
-    public static void main( String[] args )
+    public List<String> parseWorkflowXML(String xmlPath) throws DocumentException{
+        List<String> outputScripts = new ArrayList<String>();
+        SAXReader reader = new SAXReader();
+        Document document = reader.read(xmlPath);
+        
+        //Gets root element
+        Element root = document.getRootElement();
+        
+       //iterates through all the <action> tags
+        for( Iterator i = root.elementIterator("action"); i.hasNext(); ){
+            Element action = (Element) i.next();
+            
+            //Iterates through action to verify that the action is an out
+            
+        }
+        return null;
+    }
+    
+    
+    public static void main( String[] args ) throws DocumentException
     {
-        System.out.println( "Hello World!" );
+        WorkflowSpoofing ws = new WorkflowSpoofing();
+        ws.parseWorkflowXML("/home/rsuri/working/oozie-f04a4fd7-60fa-4871-8904-3b486642ec94/workflow.xml");
     }
 }
