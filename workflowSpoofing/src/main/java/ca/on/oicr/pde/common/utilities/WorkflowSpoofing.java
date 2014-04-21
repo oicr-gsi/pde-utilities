9fd96f715a82647aa89804a5bf5fa3279230e5ce
RaunaqSuri <raunaq.suri@hotmail.com>
2014-04-21 16:06:50 -0400

    commenting

--- workflowSpoofing/src/main/java/ca/on/oicr/pde/common/utilities/WorkflowSpoofing.java
+++ workflowSpoofing/src/main/java/ca/on/oicr/pde/common/utilities/WorkflowSpoofing.java
@@ -35,30 +35,34 @@ import net.sourceforge.seqware.pipeline.runner.PluginRunner;
 import org.apache.commons.io.IOUtils;
 
 public class WorkflowSpoofing {
-
+    
     private final List<String> scripts = new ArrayList<String>();
     private final Set<String> parentAccessions = new HashSet<String>();
     private String mimeType;
     private String outputFile;
     private final Map<String, String> hm = ConfigTools.getSettings();
     private final Metadata metadata = MetadataFactory.get(hm);
-
+    
     public void spoof(int workflowRunID) throws Exception {
-
+        
         int wfAccession = metadata.getWorkflowRun(workflowRunID).getWorkflowAccession();
-        String bundle = metadata.getWorkflow(wfAccession).getCwd();
         
         File iniFile = new File("tempIniFile");
-        FileUtils.writeStringToFile( iniFile ,metadata.getWorkflowRun(workflowRunID).getIniFile());
+        FileUtils.writeStringToFile(iniFile, metadata.getWorkflowRun(workflowRunID).getIniFile());
         iniFile.deleteOnExit();
         
+        
+        //Runs a seqware dry run on the workflow
         PrintStream old = System.out;
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         System.setOut(new PrintStream(baos));
         dryRun(wfAccession, iniFile.getCanonicalPath());
+        System.setOut(old);
         
+        //Gets the oozie working directory
         String dryRunOut = baos.toString();
         String oozieDir = getOozieDir(dryRunOut);
+        
         //parses the ini to get all the parent accessions the workflow accesses
         parseIniFile(metadata.getWorkflowRun(workflowRunID).getIniFile());
 
@@ -69,12 +73,12 @@ public class WorkflowSpoofing {
         fileLinkerFile.deleteOnExit();
 
         //Gets all the provision File out scripts
-        scripts.addAll(parseWorkflowXML(oozieDir+"/workflow.xml"));
+        scripts.addAll(parseWorkflowXML(oozieDir + "/workflow.xml"));
 
         //Casts the list into a set
         //Gets the file provenance report and stores it in file linker objects and write it to the file linker file
         Set<SpoofLinker> fileLinkerObjs = FileProvenanceReaderForFileLinker.readWithCsvMapReader(getFileProvenanceReport());
-
+        
         for (String script : scripts) {
             parseProvisionScript(script);
             for (SpoofLinker spoof : fileLinkerObjs) {
@@ -82,16 +86,17 @@ public class WorkflowSpoofing {
                 spoof.setMimeType(mimeType);
                 spoof.setSeparator(",");
                 FileUtils.writeStringToFile(fileLinkerFile, spoof.toString(), true);
+                System.out.println(spoof.toString());
             }
-
+            
         }
 
         //runs the file linker plugin
         //runFileLinkerPlugin(fileLinkerFile.getCanonicalPath(), String.valueOf(wfAccession));
     }
-
+    
     private void runFileLinkerPlugin(String fileLinkerPath, String wfaccession) throws IOException {
-
+        
         String[] fileLinkerParams = {"--file-list-file", fileLinkerPath, "--workflow-accession", wfaccession, "--csv-separator", ","};
         PluginRunner p = new PluginRunner();
         List<String> a = new ArrayList<String>();
@@ -100,15 +105,15 @@ public class WorkflowSpoofing {
         a.add("--");
         a.addAll(Arrays.asList(fileLinkerParams));
         System.out.println(Arrays.deepToString(a.toArray()));
-
+        
         p.run(a.toArray(new String[a.size()]));
     }
-
+    
     private StringReader getFileProvenanceReport() throws Exception {
         String userAuth = Base64.encodeBytes("admin@admin.com:admin".getBytes());
         String url = getWebserviceUrl();
         String params = "?";
-
+        
         Iterator<String> processingIter = parentAccessions.iterator();
 
         //First id
@@ -138,23 +143,23 @@ public class WorkflowSpoofing {
 
         //Gets the file provenance report
         URL reportUrl = new URL(url + "/reports/file-provenance" + params);
-
+        
         HttpURLConnection connection = (HttpURLConnection) reportUrl.openConnection();
         connection.setRequestMethod("GET");
         connection.setDoOutput(true);
         connection.setRequestProperty("Authorization", "Basic " + userAuth);
         connection.connect();
-
+        
         StringWriter reportWriter = new StringWriter();
         IOUtils.copy(connection.getInputStream(), reportWriter);
         StringReader reader = new StringReader(reportWriter.toString().trim());
         return reader;
     }
-
+    
     private void parseProvisionScript(String script) throws IOException {
-
+        
         File scriptFile = new File(script);
-
+        
         String contents = FileUtils.readFileToString(scriptFile);
         for (String individualCommand : splitIntoCommands(contents)) {
             for (String parameter : splitIntoParameters(individualCommand)) {
@@ -163,11 +168,11 @@ public class WorkflowSpoofing {
                 } else if (parameter.matches("input-file-metadata .*")) {
                     mimeType = getMimeType(parameter);
                 }
-
+                
             }
         }
     }
-
+    
     private String getWebserviceUrl() throws Exception {
         String aScript = FileUtils.readFileToString(new File(scripts.get(0)));
         String settingsPath = "";
@@ -179,7 +184,7 @@ public class WorkflowSpoofing {
         if (settingsPath.isEmpty()) {
             throw new Exception("Settings file not found");
         }
-
+        
         String settings = FileUtils.readFileToString(new File(settingsPath));
         for (String setting : settings.split("\n")) {
             if (setting.matches("SW_REST_URL=.*")) {
@@ -188,23 +193,23 @@ public class WorkflowSpoofing {
         }
         throw new Exception("Could not get webservice url");
     }
-
+    
     private String getMimeType(String parameter) {
         return parameter.substring(parameter.indexOf("::") + 2, parameter.lastIndexOf("::"));
     }
-
+    
     private String getOutputFile(String parameter) {
         return parameter.substring(parameter.indexOf(" ") + 1, parameter.lastIndexOf(" "));
     }
-
+    
     private String[] splitIntoCommands(String contents) {
         return contents.split("\n");
     }
-
+    
     private String[] splitIntoParameters(String contents) {
         return contents.split("--");
     }
-
+    
     public void parseIniFile(String iniFile) throws IOException {
         //Splits the file by newlines
         String accessions = "";
@@ -215,12 +220,12 @@ public class WorkflowSpoofing {
         }
         parentAccessions.addAll(Arrays.asList(accessions.split(",")));
     }
-
+    
     public List<String> parseWorkflowXML(String xmlPath) throws DocumentException {
         List<String> outputScripts = new ArrayList<String>();
-
+        
         List<Element> actionOut = new ArrayList<Element>();
-
+        
         SAXReader reader = new SAXReader();
         Document document = reader.read(xmlPath);
 
@@ -241,20 +246,20 @@ public class WorkflowSpoofing {
         }
         return outputScripts;
     }
-
+    
     private boolean isProvisionOut(Element action) {
-
+        
         for (Iterator i = action.attributeIterator(); i.hasNext();) {
             Attribute attribute = (Attribute) i.next();
-
+            
             if (attribute.getValue().matches("provisionFile_[oO]ut_.*")) {
                 return true;
             }
         }
         return false;
-
+        
     }
-
+    
     private String getOutputScriptFilePaths(Element action) {
         for (Iterator i = action.elementIterator("sge"); i.hasNext();) {
             Element sge = (Element) i.next();
@@ -263,18 +268,18 @@ public class WorkflowSpoofing {
                 String path = scriptPath.getData().toString();
                 return path;
             }
-
+            
         }
         return null;
     }
-
+    
     public static void main(String[] args) throws Exception {
         WorkflowSpoofing ws = new WorkflowSpoofing();
         // String[] bamParams = {"--wf-accession", "928", "--study-name", "PDE_TEST", "--schedule"};
 //        BamQCDecider.main(bamParams);
         ws.spoof(929);
     }
-
+    
     private void dryRun(int workflowAccession, String iniFilePath) {
         String[] fileLinkerParams = {"--workflow-accession", String.valueOf(workflowAccession),
             "--ini-files", iniFilePath, "--no-metadata", "--no-run"};
@@ -285,13 +290,13 @@ public class WorkflowSpoofing {
         a.add("--");
         a.addAll(Arrays.asList(fileLinkerParams));
         System.out.println(Arrays.deepToString(a.toArray()));
-
+        
         p.run(a.toArray(new String[a.size()]));
     }
-
+    
     private String getOozieDir(String dryRunOut) throws Exception {
-        for(String s : dryRunOut.split("\n")){
-            if(s.matches("^Using working directory:.*$")){
+        for (String s : dryRunOut.split("\n")) {
+            if (s.matches("^Using working directory:.*$")) {
                 String[] t = s.split(": ");
                 return t[1];
             }
